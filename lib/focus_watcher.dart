3817561fb470edde0cd189a7b8e77308ec82c1ae
nirmal0707 <nirmalkk0707@gmail.com>
2024-09-29 18:32:46 +0530

    feat: init package

---
+++ lib/focus_watcher.dart
@@ -0,0 +1,3 @@
+library focus_watcher;
+
+export 'src/focus_watcher_widget.dart';
---
+++ lib/src/focus_watcher_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class FocusWatcher extends StatelessWidget {
+  const FocusWatcher({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
